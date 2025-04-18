60d9cc28178d09d963a49db29b153a3bcc9eb56f
Ryan Appel <120493240@fvtc.edu>
2025-04-17 20:25:10 -0500

    :sparkles: added client-side safe fetch

---
+++ public/scripts/course-copy.js
@@ -0,0 +1,19 @@
+
+import { safeFetch } from "./safe-fetch.js"
+
+(async () => {
+    const p = document.querySelector('p')
+    const courseId = 'test-course-id'
+    const name = 'test-course-name'
+
+    const { result, error } = await safeFetch('/api/v1/courses/copy', {
+		method: 'POST',
+		headers: { 'Content-Type': 'application/json' },
+		body: JSON.stringify({ courseId, name })
+	})
+    if (error) return p.textContent = JSON.stringify(error, null, 2)
+
+    const json = await result.json()
+
+    p.textContent = JSON.stringify(json, null, 2)
+})()
